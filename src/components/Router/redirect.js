dd72b2d60d19b8bd9e88b796a76de086061e608e
Luciana Colombo <lcolombo@atixlabs.com>
2020-02-28 14:21:19 -0300

    feat: Added router component

---
+++ src/components/Router/redirect.js
@@ -0,0 +1,26 @@
+/* eslint-disable consistent-return */
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createQueryString } from '../../services/api-calls/helpers';
+
+export const useRedirect = () => {
+  const [urlToRedirect, setUrlToRedirect] = useState();
+
+  const redirect = (queryParameters = {}) => {
+    const processedQueryParameters = `?${createQueryString(queryParameters)}`;
+    if (urlToRedirect) {
+      return (
+        <Redirect
+          to={{
+            pathname: urlToRedirect,
+            search: processedQueryParameters
+          }}
+        />
+      );
+    }
+  };
+
+  return { redirect, setUrlToRedirect };
+};
+
+export default useRedirect;
---
+++ src/components/Router/router.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, BrowserRouter } from 'react-router-dom';
+import PrivateRoute from './private-route';
+import Login from '../pages/Login/login';
+import Error404 from '../pages/Error404/error404';
+
+
+const routesConfig = [
+  { component: Error404, requireAuthentication: false }
+];
+
+export const Router = () => {
+  const routes = routesConfig.map((route, index) => <PrivateRoute key={index} {...route} />);
+  return (
+    <div className="AppContainer">
+      <div className="MainContent">
+        <div>
+          <BrowserRouter>
+            <Switch>{routes}</Switch>
+          </BrowserRouter>
+        </div>
+      </div>
+    </div>
+  );
+};
