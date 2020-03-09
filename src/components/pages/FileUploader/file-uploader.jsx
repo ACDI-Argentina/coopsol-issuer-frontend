97f32be1ba157bda28105f9b6212edf6cadea914
Luciana Colombo <lcolombo@atixlabs.com>
2020-03-09 09:57:35 -0300

    feat: Uses antd Upload component to upload files
    At the time, it only supports .xlsx files

--- src/components/pages/FileUploader/file-uploader.jsx
+++ src/components/pages/FileUploader/file-uploader.jsx
@@ -1,45 +1,70 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { UserContext } from '../../../services/providers/user-context';
-import apiCalls from '../../../services/api-calls/all';
-import AntForm from '../../molecules/ant-form';
-import { processedErrorMessage } from '../../../services/api-calls/helpers';
-import { useRedirect } from '../../Router/redirect';
-import { HOME_URL } from '../../../utils/constants';
-import {Progress} from 'reactstrap';
-import { FilePond } from 'react-filepond';
-import 'filepond/dist/filepond.min.css';
+import React, { useState } from "react";
+import apiCalls from "../../../services/api-calls/all";
+import { processedErrorMessage } from "../../../services/api-calls/helpers";
+import { Upload, Icon, message } from "antd";
+import ButtonPrimary from "../../atoms/ButtonPrimary/button-primary";
 //import './_style.scss';
 //import '../../../css/app.scss';
 
 const { uploadFile } = apiCalls();
-
-
+const { Dragger } = Upload;
 
 const FileUploader = () => {
-    const { user, setUser } = useContext(UserContext);
-    const [errorMessage, setErrorMessage] = useState();
+  const [file, setFile] = useState(null);
+  const [responseStatus, setResponseStatus] = useState({});
 
-    return(
+  const handleSubmit = async () => {
+    try {
+      const formData = new FormData();
+      formData.set("file", file);
+      const response = await uploadFile(formData);
+      setResponseStatus(response.status);
+      message.success("El archivo se ha subido correctamente");
+    } catch (error) {
+      const errorMessage = processedErrorMessage(error);
+      message.error(errorMessage);
+    }
+  };
+
+  const props = {
+    name: "file",
+    accept: ".xlsx",
+    multiple: false,
+    beforeUpload: fileTemporary => {
+      setFile(fileTemporary);
+      return false;
+    }
+  };
+
+  return (
     <div>
-     <AntForm inputs={} handleSubmit={upload} submitText={'Upload'} />
-    <FilePond ref={ref => this.pond = ref} />
+      <div className="mainSectionReport">
+        <div className="UploadReport">
+          <div className="title">
+            <h2>Upload Report</h2>
+            <div className="buttonSection">
+              <ButtonPrimary
+                text="Upload"
+                theme="ThemePrimary"
+                onClick={handleSubmit}
+              />
+            </div>
+          </div>
+          <div className="preview">
+            <Dragger {...props}>
+              <p className="ant-upload-drag-icon">
+                <Icon type="inbox" />
+              </p>
+              <p className="ant-upload-text">
+                Haga clic o arrastre un archivo a este área para cargar
+              </p>
+              <p className="ant-upload-hint">Formatos aceptados: .xlsx</p>
+            </Dragger>
+          </div>
+        </div>
+      </div>
     </div>
-  /* // y tengo que ver una forma de pasarle al AntForm como inputs el "this.pond.getFiles()" */
-      );
-
-
-    const upload = async values => {
-      try {
-        const response = await uploadFile(values);
-        setUser(response.data);
-      } catch (error) {
-        const errorMessage = processedErrorMessage(error);
-        setErrorMessage(errorMessage);
-      }
-    };
+  );
 };
 
 export default FileUploader;
-
-
-
