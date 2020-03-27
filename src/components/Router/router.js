d4052e1291c0ce8d2246e40175adba4b3bcb93cc
Nicolas Stupfler <nstupfler@atixlabs.com>
2020-03-27 10:20:52 -0300

    SEMI-57 fix package json + routing

--- src/components/Router/router.js
+++ src/components/Router/router.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Switch, BrowserRouter } from 'react-router-dom';
 import PrivateRoute from './private-route';
+
 import Login from '../pages/Login/login';
+import Home from '../pages/Home/home';
 import FileUploader from '../pages/FileUploader/file-uploader';
 import Error404 from '../pages/Error404/error404';
 
+import { HOME_URL, LOGIN_URL, UPLOAD_FILE_URL } from '../../utils/constants';
 
 const routesConfig = [
-  { path: '/login', component: Login, requireAuthentication: false },
-  { path: '/upload', component: FileUploader, requireAuthentication: true },
+  {
+    path: LOGIN_URL,
+    component: Login,
+    requireAuthentication: false,
+    exact: true
+  },
+  { path: HOME_URL, component: Home, requireAuthentication: true, exact: true },
+  {
+    path: UPLOAD_FILE_URL,
+    component: FileUploader,
+    requireAuthentication: true,
+    exact: true
+  },
   { component: Error404, requireAuthentication: false }
 ];
 
