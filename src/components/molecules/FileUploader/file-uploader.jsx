7b6c7f93333c0b70f323a73f9b4c9a3a3f73f660
Axel Bautista <abautista@atixlabs.com>
2020-04-01 16:54:56 -0300

    File uplaod controller

--- src/components/molecules/FileUploader/file-uploader.jsx
+++ src/components/molecules/FileUploader/file-uploader.jsx
@@ -3,6 +3,7 @@ import apiCalls from '../../../services/api-calls/all';
 import { processedErrorMessage } from '../../../services/api-calls/helpers';
 import { Upload, message } from 'antd';
 import ButtonPrimary from '../../atoms/ButtonPrimary/button-primary';
+import './_style.scss';
 
 const { uploadFile } = apiCalls();
 const { Dragger } = Upload;
@@ -10,6 +11,9 @@ const { Dragger } = Upload;
 const FileUploader = () => {
   const [file, setFile] = useState(null);
   const [responseStatus, setResponseStatus] = useState({});
+  const [showContainer, setShowContainer] = useState(false);
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async () => {
     try {
@@ -17,10 +21,10 @@ const FileUploader = () => {
       formData.set('file', file);
       const response = await uploadFile(formData);
       setResponseStatus(response.status);
-      message.success('El archivo se ha subido correctamente');
+      setSuccess('Archivo subido correctamente!');
     } catch (error) {
       const errorMessage = processedErrorMessage(error);
-      message.error(errorMessage);
+      setError(errorMessage);
     }
   };
 
@@ -34,24 +38,38 @@ const FileUploader = () => {
     }
   };
 
+  const onFileChanged = data => {
+    setError(null);
+    setSuccess(null);
+    setShowContainer(data.fileList.length > 0);
+  };
+
   return (
-    <div>
+    <div className="FileUploader">
       <div className="mainSectionReport">
         <div className="UploadReport">
-          <div className="title">
-            <h2>Upload Report</h2>
-            <div className="buttonSection">
-              <ButtonPrimary text="Upload" theme="ThemePrimary" onClick={handleSubmit} />
-            </div>
-          </div>
           <div className="preview">
-            <Dragger {...props}>
-              <p className="ant-upload-drag-icon">{/* <Icon type="inbox" /> */}</p>
+            <Dragger {...props} onChange={onFileChanged} className={showContainer ? 'hidden' : ''}>
               <p className="ant-upload-text">
                 Haga clic o arrastre un archivo a este área para cargar
               </p>
               <p className="ant-upload-hint">Formatos aceptados: .xlsx</p>
             </Dragger>
+
+            <div className="title">
+              <div className="message">
+                {error && <div className="success">{error}</div>}
+                {success && <div className="error">{success}</div>}
+              </div>
+              <div className="buttonSection">
+                <ButtonPrimary
+                  text="Upload"
+                  theme="ThemePrimary"
+                  onClick={handleSubmit}
+                  disabled={!showContainer}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/AddCredentials/add-credentials.jsx
+++ src/components/pages/AddCredentials/add-credentials.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './_style.scss';
 
 import api from '../../../services/api-calls/all';
+import FileUploader from '../../molecules/FileUploader/file-uploader';
 
 // const { endpoint } = api();
 
 const AddCredentials = props => {
-  return <div className="Credentials">AddCredentials</div>;
+  const onSubmitted = () => {};
+
+  return (
+    <div className="Credentials">
+      <FileUploader onSubmitted={onSubmitted} />
+    </div>
+  );
 };
 
 export default AddCredentials;
