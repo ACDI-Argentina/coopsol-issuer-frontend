41e6e9c1c47545a31cf1ea74bca6b2daee124b54
Axel Bautista <abautista@atixlabs.com>
2020-04-01 12:45:30 -0300

    added basic navigation to upload page

--- src/components/molecules/SimpleHeader/simple-header.jsx
+++ src/components/molecules/SimpleHeader/simple-header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './_style.scss';
 import '../../../css/app.scss';
+import { Button } from 'antd';
+import history from '../../Router/history';
 
 const SimpleHeader = () => {
-  return <div className="SimpleHeader"></div>;
+  const onBack = () => {
+    history.goBack();
+  };
+
+  return (
+    <div className="SimpleHeader">
+      <Button onClick={onBack}>Volver</Button>
+    </div>
+  );
 };
 
 export default SimpleHeader;
