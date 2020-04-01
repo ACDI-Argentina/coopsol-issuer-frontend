a51e80f756205fe8898aa61a2597be87ca78d430
Axel Bautista <abautista@atixlabs.com>
2020-04-01 11:01:22 -0300

    Added support for pages with different headers

---
+++ src/components/molecules/SimpleHeader/simple-header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './_style.scss';
+import '../../../css/app.scss';
+
+const SimpleHeader = () => {
+  return <div className="SimpleHeader"></div>;
+};
+
+export default SimpleHeader;
---
+++ src/components/pages/AddCredentials/add-credentials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './_style.scss';
+
+import api from '../../../services/api-calls/all';
+
+// const { endpoint } = api();
+
+const AddCredentials = props => {
+  return <div className="Credentials">AddCredentials</div>;
+};
+
+export default AddCredentials;
