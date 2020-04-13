d4d50891fa2a3b1ade9be52aaf489422ce20893e
VaniJelavich <vanina.jelavich@gmail.com>
2020-04-13 09:15:32 -0300

    feat(Styles-on-activities-page): add styles on activities page

--- src/components/atoms/TitlePage/TitlePage.jsx
+++ src/components/atoms/TitlePage/TitlePage.jsx
@@ -3,7 +3,7 @@ import './_style.scss';
 import ButtonPrimary from '../ButtonPrimary/button-primary';
 import { ADD_CREDENTIALS } from '../../../utils/constants';
 
-const TitlePage = ({ history }) => {
+const TitlePage = ({ history, text, description }) => {
   const addCredential = () => {
     history.push(ADD_CREDENTIALS);
   };
@@ -11,12 +11,11 @@ const TitlePage = ({ history }) => {
   return (
     <div className="TitlePage">
       <div className="title">
-        <h1>Credenciales</h1>
+        <h1>{text}</h1>
         <ButtonPrimary onClick={addCredential} text="+ Crear nueva credencial" theme="primary" />
       </div>
       <p className="subtitle">
-        Acá podés crear credenciales, precredenciales, ver el listados de las que se encuentran
-        generadas y las que están pendientes.
+        {description}
       </p>
     </div>
   );
--- src/components/pages/Activities/activities.jsx
+++ src/components/pages/Activities/activities.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './_style.scss';
-
+import TitlePage from '../../atoms/TitlePage/TitlePage';
 import api from '../../../services/api-calls/all';
 
 // const { endpoint } = api();
 
 const Activities = () => {
-  return <div className="Activities">Activities Page</div>;
+  return (
+    <div className="Activities">
+      <TitlePage
+        text="Listado de actividades"
+        description="Acá podés ver un listado con las actividades de los usuarios."
+      />
+      <div className="ActivitiesContent">
+        <h4>
+          <img src="img/table-list.svg" /> Listado de actividades
+        </h4>
+        <div className="TableContent">
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Activities;
