2ba510c46aaf17eb7c096f8e4f2f7c15452b49a4
VaniJelavich <vanina.jelavich@gmail.com>
2020-04-13 10:46:42 -0300

    fix(Styles-on-requests-page): Styles fixies on requests page

--- src/components/pages/Requests/requests.jsx
+++ src/components/pages/Requests/requests.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './_style.scss';
-
+import TitlePage from '../../atoms/TitlePage/TitlePage';
 import api from '../../../services/api-calls/all';
+import CredentialTable from '../../molecules/CredentialTable/credential-table';
 
 // const { endpoint } = api();
 
 const Requests = () => {
-  return <div className="Credentials">Requests Page</div>;
+  return (
+    <div className="Requests">
+      <TitlePage
+        text="Solicitud de credenciales"
+        description="Acá podés ver un listado con las solicitudes de credenciales."
+      />
+      <div className="RequestsContent">
+        <h4>
+          <img src="img/table-list.svg" /> Listado de solicitudes
+        </h4>
+        <CredentialTable /> 
+      </div>
+    </div>
+  );
 };
 
 export default Requests;
