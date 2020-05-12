2229755ca860b917528edbe6f41639636cf28a17
uribrama <uribrama@gmail.com>
2020-05-12 15:49:26 -0300

    fix: urls from credential

--- src/services/api-calls/user.api-calls.js
+++ src/services/api-calls/user.api-calls.js
@@ -1,15 +1,15 @@
 import helpers from './helpers';
 
-const getCredentials = makeGetRequest => data => makeGetRequest('credential', data);
+const getCredentials = makeGetRequest => data => makeGetRequest('credentials', data);
 
 const revokeCredentials = makePatchRequest => data =>
   makePatchRequest('credential/revoke/{id}', data);
 
-const getCredentialTypes = makeGetRequest => () => makeGetRequest('credential/credentialTypes');
+const getCredentialTypes = makeGetRequest => () => makeGetRequest('credentials/types');
 
-const getCredentialStates = makeGetRequest => () => makeGetRequest('credential/credentialStates');
+const getCredentialStates = makeGetRequest => () => makeGetRequest('credentials/states');
 
-const getCredentialStatus = makeGetRequest => () => makeGetRequest('credential/credentialStatus');
+const getCredentialStatus = makeGetRequest => () => makeGetRequest('credentials/status');
 
 export default client => {
   const { makePostRequest, makeGetRequest, makePatchRequest, makeDeleteRequest } = helpers(client);
