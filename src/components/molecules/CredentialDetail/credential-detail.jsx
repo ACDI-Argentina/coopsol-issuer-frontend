3dd1591585160f16b8ca81da06c0923db16179ce
Nicolas Laitan <nlaitan@atixlabs.com>
2020-06-01 20:30:16 -0300

    feat: detail component

---
+++ src/components/molecules/CredentialDetail/credential-detail.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './_style.scss';
+
+const CredentialDetail = ({ record }) => {
+
+  
+  return (
+    <div className="CredentialDetail">
+      {/*  Aca va la info a mostrar  */}
+    </div>
+  );
+};
+
+export default CredentialDetail;
