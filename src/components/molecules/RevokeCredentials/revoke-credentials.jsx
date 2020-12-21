aff75709eee84ab358a168bb567001362367cb4b
Facundo Cetraro <fcetraro@atixlabs.com>
2020-12-21 15:43:12 -0300

    Feat: Add name, dni and credential type to revocation modal

--- src/components/molecules/RevokeCredentials/revoke-credentials.jsx
+++ src/components/molecules/RevokeCredentials/revoke-credentials.jsx
@@ -117,7 +117,9 @@ const RevokeCredentials = ({ credential, onRevoked, reasonId }) => {
         </div>
         <div className="body">
           <p>
-            ¿Confirma la revocación de la credencial?
+            Esta a punto de revocar la credencial de <span className="bold-text">{credential.name}</span>
+            <br />
+            con DNI <span className="bold-text"> {credential.dniBeneficiary}</span> de tipo <span className="bold-text"> {credential.credentialType}</span>
           </p>
           {showWarning()}
         </div>
