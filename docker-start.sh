d535c324d7bf2fee08cde4148b6ad40715b791a6
Matias Oveja Smith <matias.ovejasmith@bitcoinargentina.org>
2021-07-20 23:04:08 -0300

    Fix environment vars to work in dev and docker modes

--- docker-start.sh
+++ docker-start.sh
@@ -4,5 +4,6 @@
 # https://www.freecodecamp.org/news/how-to-implement-runtime-environment-variables-with-create-react-app-docker-and-nginx-7f9d42a91d70/
 
 echo "window._env_ = \"$ENVIRONMENT\"" > /var/www/config/env.js;
+echo "window._ikey_ = \"$REACT_APP_INSIGTHS_IKEY\"" >> /var/www/config/env.js;
 
 http-server-spa /var/www index.html 8080
